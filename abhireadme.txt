9f019d2376c48b638409dc357db2a7030b122cfc
abhigeorge <abhigeorgekalladayil@gmail.com>
2023-03-29 19:15:32 +0530

    first commit

---
+++ abhireadme.txt
@@ -0,0 +1,5 @@
+
+
+Based on this Youtube Videos Tuts: https://youtu.be/iVThaG_sAt0?t=1599
+
+Part One is complated : https://youtu.be/iVThaG_sAt0
---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<x-app-layout meta-title="The Super Blog" meta-description="Lorem ipsum dolor sit amet, consectetur adipisicing elit">
+    <div class="container mx-auto flex flex-wrap py-6">
+
+        <!-- Posts Section -->
+        <section class="w-full md:w-2/3  px-3">
+            <div class=" flex flex-col items-center">
+                @foreach($posts as $post)
+                    <x-post-item :post="$post"/>
+                @endforeach
+            </div>
+            {{ $posts->links() }}
+        </section>
+
+        <!-- Sidebar Section -->
+        <x-sidebar />
+
+    </div>
+</x-app-layout>
